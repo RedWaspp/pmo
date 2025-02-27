b60c69539ceed13fe8c7f12c8618fab8e5969c30
RedWaspp <redwaspunity@gmail.com>
2025-02-27 13:12:46 -0500

    Comments removed

--- src/main/java/com/pmo/controller/PMOController.java
+++ src/main/java/com/pmo/controller/PMOController.java
@@ -5,19 +5,19 @@ import com.pmo.model.State;
 import com.pmo.model.Vehicle;
 import com.pmo.service.StateService;
 import com.pmo.service.VehicleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class PMOController {
+    //Never  use @Autowired instead use Constructor Injection
     final VehicleService vehicleService;
     final StateService StateService;
 
-    public PMOController(VehicleService vehicleService, com.pmo.service.StateService stateService, LargestEx largestEx)
-    {
-
+    //Constructor Injection
+    public PMOController(VehicleService vehicleService, com.pmo.service.StateService stateService) {
         this.vehicleService = vehicleService;
         StateService = stateService;
     }
